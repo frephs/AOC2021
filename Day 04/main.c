6b1cdfa68a4aa9d6677c101e5c2b0c6e0b293fc9
Francesco <ffrancescogenovese@gmail.com>
2021-12-07 01:13:30 +0100

    Day 04

--- Day 04/main.c
+++ Day 04/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define FN "input.txt"
-#define MAXCHAREXT 9+2*90+100
+#define MAXCHAREXT 291
 #define NROWS 5
-#define NCOL 5
+#define NCOLS 5
 
 #define FN "input.txt"
 
 typedef struct bingo_{
-     int card[NROWS][NCOL];
+     int card[NROWS][NCOLS];
      int won;
      struct bingo_ *next;
 }bingo_t;
@@ -17,24 +18,79 @@ typedef struct extractions_{
      struct extractions_ *next;
 }extractions_t;
 
-//Lists are redundant but were implemented to exercise for my FDI exam
+
+FILE *getExtractions(char filename[], extractions_t **hExt);
+bingo_t *getBingoCards(FILE *fp, int nrows, int ncols);
+
+void printBingoCards(bingo_t *hCards,  int nrows, int ncols);
+void printExtractions(extractions_t *ext);
+
+int won(bingo_t **currcard, int nrows, int ncols);
+void extractNumbers(extractions_t *ext, bingo_t *hCards, int nrows, int ncols);
+int calcScore(bingo_t *curr, extractions_t *ext, int nrows, int ncols);
+
+void checkFirstWinning(extractions_t *ext, bingo_t *hCards, int nrows, int ncols, int *score);
+int checkLastWinning(extractions_t *ext, bingo_t *hCards, int nrows, int ncols);
+
+
+
+
+bingo_t *duplicateCards(bingo_t *hCards, bingo_t *duplicate);
 
 int main(){
-     extractions_t *hExt;
-     getExtractions(FN, *hExt);
+     extractions_t *hExt, *currExt;
+     bingo_t *hCards, *firstCheck, *secondCheck, *minWin, *currCard;
+     FILE *fp;
+     int score, allWon;
+
+     fp = getExtractions(FN, &hExt);
+     hCards = getBingoCards(fp, NROWS, NCOLS);
+
+     //printBingoCards(hCards, NROWS, NCOLS);
+     //removing the first blank card
+
+     hCards = hCards->next;
+     //printExtractions(hExt);
+
+     //Duplicating the do the first extraction
+     firstCheck = duplicateCards(hCards, firstCheck);
+     for(currExt = hExt, score=0; currExt && !score; currExt = currExt->next){
+          extractNumbers(currExt, firstCheck, NROWS, NCOLS);
+          checkFirstWinning(currExt, firstCheck, NROWS, NCOLS, &score);
+     }
+     printf("Score: %d\n", score);
+
+     secondCheck = duplicateCards(hCards, secondCheck);
+
+     for(currExt = hExt, allWon =0 ; currExt && !allWon; currExt= currExt->next){
+          extractNumbers(currExt, secondCheck, NROWS, NCOLS);
+          //printf("\n\nEstratto %d: \n\n", currExt->ext);
+          allWon = checkLastWinning(currExt, secondCheck, NROWS, NCOLS);
+          //printBingoCards(secondCheck, NROWS, NCOLS);
+          if(allWon){
+               for(currCard = secondCheck, minWin= currCard; currCard; currCard = currCard->next){
+                    if(currCard->won<=minWin->won){
+                         minWin = currCard;
+                    }
+               }
+               printBingoCards(secondCheck, NROWS,NCOLS);
+               score = calcScore(minWin,currExt, NROWS,NCOLS);
+          }
+     }
+     printf("Score: %d\n", score);
 
 }
 
-FILE *getExtractions(char filename[], extractions_t *hExt){
+FILE *getExtractions(char filename[], extractions_t **hExt){
      FILE *fp;
-     char exts[MAXCHAREXT];
-     int n, pow;
+     char exts[MAXCHAREXT+1];
+     int n, pow, len, i;
      extractions_t *tmp, *curr;
-     hExt = NULL;
+     *hExt = NULL;
      fp = fopen(filename, "r");
      if(fp){
-          fgets(fp, MAXCHAREXT, exts);
-          for(len=0; exts[len]!='\0';len++);
+          fgets(exts, MAXCHAREXT+1, fp);
+          for(len=0; exts[len]!='\0' && exts[len]!=' ' && exts[len]!=13 && exts[len]!=10 && exts[len]!='\n';len++);
           for(i=len-1, n=0, pow=1; i>=0; i--){
                if(exts[i]!=','){
                     n+= (exts[i] - '0')*pow;
@@ -45,13 +101,12 @@ FILE *getExtractions(char filename[], extractions_t *hExt){
                     tmp = malloc(sizeof(extractions_t));
                     if(tmp){
                          tmp->ext = n;
-                         if(!hExt){
-                              hExt=tmp;
-                              hExt->next = NULL;
+                         if(!(*hExt)){
+                              *hExt = tmp;
+                              (*hExt)->next = NULL;
                          }else{
-                              for(curr = *hExt;curr->next;curr=curr->next);
-                              curr->next = tmp;
-                              tmp->next = NULL;
+                              tmp->next = *hExt;
+                              *hExt = tmp;
                          }
                     }else{
                          printf("Could not allocate memory\n");
@@ -60,11 +115,168 @@ FILE *getExtractions(char filename[], extractions_t *hExt){
                }
 
           }
-          for(curr=hExt;curr;curr=curr->next){
-               printf("%d -> ", curr->ext);
+          tmp = malloc(sizeof(extractions_t));
+          if(tmp){
+               tmp->ext = n;
+               tmp->next = *hExt;
+               *hExt = tmp;
           }
+
      }else{
-          printf("Could not open the file")
+          printf("Could not open the file");
      }
      return fp;
+}
+
+bingo_t *getBingoCards(FILE *fp, int nrows, int ncols){
+     bingo_t *hCards, *tmp;
+     int i, j, k=0;
+     hCards = NULL;
+
+     if(fp){
+          //assuming all bingo cards in the input file are valid
+          while(!feof(fp)){
+               tmp = malloc(sizeof(bingo_t));
+               tmp->won = 0;
+               if(tmp){
+                    for(i=0;i<nrows;i++){
+                         for(j=0;j<ncols;j++){
+                              fscanf(fp, "%d", &(tmp->card)[i][j]);
+                         }
+                    }
+                    if(!hCards){
+                         hCards = tmp;
+                         hCards->next = NULL;
+                    }else{
+                         tmp->next = hCards;
+                         hCards= tmp;
+                    }
+               }else{
+                    printf("Could not allocate memory");
+               }
+          }
+     }
+     fclose(fp);
+     return hCards;
+}
+
+
+void printBingoCards(bingo_t *hCards, int nrows, int ncols){
+     int i,j;
+     bingo_t *curr;
+     for(curr = hCards; curr; curr=curr->next){
+          for(i=0;i<nrows;i++){
+               for(j=0;j<ncols;j++){
+                    printf("%2d ", (curr->card)[i][j]);
+               }
+               printf("\n");
+          }
+          printf("Wins: %d\n", curr->won);
+     }
+}
+
+void printExtractions(extractions_t *ext){
+     for(ext; ext; ext=ext->next){
+          printf("%d -> ", ext->ext);
+     }
+     printf("\n");
+}
+
+
+int won(bingo_t **currcard, int nrows, int ncols){
+     int i, j, count;
+     for(i=0;i<nrows && !((*currcard)->won);i++){
+          for(j=0, count=0;j<ncols;j++){
+               if((*currcard)->card[i][j] >= 100 )
+                    count++;
+          }
+          if(count==ncols)
+               (*currcard)->won = 1;
+     }
+     for(i=0;i<ncols && !((*currcard)->won);i++){
+          for(j=0, count=0;j<nrows;j++){
+               if((*currcard)->card[j][i] >=100 ){
+                    count++;
+
+               }
+          }
+          if(count==ncols)
+               (*currcard)->won = 1;
+     }
+     return (*currcard)->won;
+}
+
+
+void extractNumbers(extractions_t *ext, bingo_t *hCards, int nrows, int ncols){
+     bingo_t * curr;
+     int i, j;
+     for(curr=hCards; curr; curr = curr->next){
+          for(i=0;i<nrows;i++){
+               for(j=0;j<ncols;j++){
+                    if(curr->card[i][j]==ext->ext){
+                         curr->card[i][j] +=100;
+                    }
+               }
+          }
+
+     }
+}
+void checkFirstWinning(extractions_t *ext, bingo_t *hCards, int nrows, int ncols, int *score){
+     bingo_t *curr;
+     int i, j, foundWinner;
+     for(curr=hCards, foundWinner=0; curr && !foundWinner; curr = curr->next){
+          if(won(&curr, nrows, ncols)){
+               foundWinner=1;
+               *score = calcScore(curr, ext, nrows, ncols);
+          }
+     }
+}
+
+
+bingo_t *duplicateCards(bingo_t *hCards, bingo_t *duplicate){
+     bingo_t *currCard, *tmp;
+     duplicate = NULL;
+     for(currCard = hCards; currCard; currCard = currCard->next){
+          tmp = malloc(sizeof(bingo_t));
+          if(tmp){
+               *tmp = *currCard; //copying all the struct
+               if(!duplicate){
+                    duplicate = tmp;
+                    duplicate->next = NULL;
+               }else{
+                    tmp->next = duplicate;
+                    duplicate = tmp;
+               }
+          }else{
+               printf("COuld not allocate memory");
+          }
+     }
+     return duplicate;
+}
+
+int checkLastWinning(extractions_t *ext, bingo_t *hCards, int nrows, int ncols){
+     bingo_t *curr;
+     int i, j, allWon;
+     for(curr=hCards, allWon=1; curr ; curr = curr->next){
+          if(won(&curr, nrows, ncols)){
+               (curr->won)++;
+          }else{
+               allWon = 0;
+          }
+     }
+     return allWon;
+}
+
+int calcScore(bingo_t *curr, extractions_t *ext, int nrows, int ncols){
+     int i, j, score;
+
+     for(i=0, score=0; i<nrows; i++){
+          for(j=0;j<ncols;j++){
+               if(curr->card[i][j]<100)
+               score += curr->card[i][j];
+          }
+     }
+     printf("Score1: %d*%d\n", score, ext->ext);
+     score *= ext->ext;
+     return score;
 }
