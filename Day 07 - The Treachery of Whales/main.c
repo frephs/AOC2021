2d4b0c858e47531361c38263b756487627961a20
Francesco <ffrancescogenovese@gmail.com>
2021-12-13 22:20:23 +0100

    Day 07

---
+++ Day 07 - The Treachery of Whales/main.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define FN "input.txt"
+
+typedef struct pos_{
+     int h;
+     struct pos_ *next;
+}pos_t;
+
+pos_t  *getInitialPositions(char filename[]);
+int getTotalFuelForPosition(pos_t *crabs, int h, int linear);
+int getNonLinearFuel(int distance);
+
+
+int main(){
+     pos_t *crabs, *curr;
+     int fuelAtCurrCrab, minFuel;
+     crabs = getInitialPositions(FN);
+     minFuel = getTotalFuelForPosition(crabs, crabs->h,1);
+     for(curr=crabs->next; curr;curr=curr->next){
+          fuelAtCurrCrab = getTotalFuelForPosition(crabs, curr->h, 1);
+          if(fuelAtCurrCrab<minFuel)
+               minFuel = fuelAtCurrCrab;
+     }
+     printf("\nMinFuel with Linear consumption: %d", minFuel);
+     minFuel = getTotalFuelForPosition(crabs, crabs->h,0);
+     for(curr=crabs->next; curr;curr=curr->next){
+          fuelAtCurrCrab = getTotalFuelForPosition(crabs, curr->h, 0);
+          if(fuelAtCurrCrab<minFuel)
+               minFuel = fuelAtCurrCrab;
+     }
+     printf("\nMinFuel with NonLinear consumption: %d", minFuel);
+
+}
+
+
+pos_t  *getInitialPositions(char filename[]){
+     pos_t *crabs, *tmp;
+     FILE *fp;
+     int n;
+     crabs = NULL;
+     fp = fopen(filename, "r");
+     if(fp){
+          fscanf(fp, "%d,", &n);
+          while(!feof(fp)){
+               tmp = malloc(sizeof(pos_t));
+               tmp->h = n;
+               if(tmp){
+                    if(!crabs){
+                         tmp->next = NULL;
+                         crabs = tmp;
+                    }else{
+                         tmp->next = crabs;
+                         crabs = tmp;
+
+                    }
+               }else{
+                    printf("Could not allocate memory");
+               }
+               fscanf(fp, "%d,", &n);
+          }
+          tmp = malloc(sizeof(pos_t));
+          if(tmp){
+               tmp->h = n;
+               tmp->next = crabs;
+               crabs = tmp->next;
+          }
+          fclose(fp);
+     }else{
+          printf("Could not open the file");
+     }
+     return crabs;
+}
+
+int getTotalFuelForPosition(pos_t *crabs, int h, int linear){
+     int totFuel;
+     for(totFuel = 0;crabs; crabs = crabs->next){
+          if(linear)
+               totFuel += abs(crabs->h - h);
+          else{
+               totFuel += getNonLinearFuel(abs(crabs->h - h -1));
+          }
+     }
+     return totFuel;
+}
+
+int getNonLinearFuel(int distance){
+     int step, fuel;
+     for(step = 1, fuel =0; distance; step++, distance--)
+          fuel += step;
+     return fuel;
+}
